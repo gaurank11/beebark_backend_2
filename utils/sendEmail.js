e54275528172f16eb72b43cb5ce9cb6fe1cc50e2
gaurank11 <sharmagaurank63@gmail.com>
2025-05-14 14:23:12 +0530

    final commit

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -9,9 +9,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
-  tls: {
-    rejectUnauthorized: false, // <-- Accept self-signed certificate
-  },
+  // tls: {
+  //   rejectUnauthorized: false, // <-- Accept self-signed certificate
+  // },
 });
 
 const sendEmail = async (to, subject, htmlContent) => {
